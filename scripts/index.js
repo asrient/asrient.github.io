0230b3c78545006344ad14452bcd2988e8bc2c3a
Asrient <asen182000@hotmail.com>
2020-12-01 21:53:40 +0530

    updated projects

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,8 @@ var screenHt = window.innerHeight;
 var s1_hi_tween = TweenMax.to("#hi", 1, { 'opacity': 0.4, scale: 0.7, top:'-60px' });
 var s1_intro_tween = TweenMax.to("#intro", 1, { 'opacity': 1, scale: 1.2, top:'60px' });
 var s2_bg_tween = TweenMax.to("#bg", 1, { 'background-color': 'rgb(0, 23, 38)' });
-var s3_bg_tween = TweenMax.to("#bg", 1, { 'background-color': 'rgb(38, 27, 0)' });
-var s2_bar_tween = TweenMax.to("#s2box", 1, { 'background-color': 'rgb(38, 27, 0)', width: '100vw', height: '100vh' });
+var s3_bg_tween = TweenMax.to("#bg", 1, { 'background-color': 'rgb(18, 0, 20)' });
+var s2_bar_tween = TweenMax.to("#s2box", 1, { 'background-color': 'rgb(18, 0, 20)', width: '100vw', height: '100vh' });
 
 // build scene
 var s1_hi_scene = new ScrollMagic.Scene({ triggerElement: "#s1", triggerHook: 0, duration: screenHt/4, offset: 0 })
