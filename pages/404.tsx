77eeea7cc548e0d30c3c9262bc25a9659eaf8df5
asrient <44570278+asrient@users.noreply.github.com>
2023-06-16 02:18:25 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ export default function Custom404() {
         <Head>
             <title>404 - Page not found</title>
         </Head>
-        <h1 className="text-2xl text-black flex flex-col text-center items-center pt-32">
+        <h1 className="text-2xl text-black dark:text-white flex flex-col text-center items-center pt-32">
             <div className='text-8xl text-gray-600 font-thin leading-relaxed'>404</div>
             <div>Sorry, we couldn't find the page you are looking for.</div>
         </h1>
