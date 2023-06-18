0670f353210dbd62166695759cbffd32da569327
asrient <44570278+asrient@users.noreply.github.com>
2023-06-18 16:15:20 +0530

    Update post-preview.tsx

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -21,7 +21,7 @@ const PostPreview = ({
   return (
     <div>
       <div className="pb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} full={false} />
+        {/* <CoverImage slug={slug} title={title} src={coverImage} full={false} /> */}
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
